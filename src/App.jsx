26397a0c7a78a747a2e86275098cdff17526f99d
Shiruvati <mrnattyboy05@gmail.com>
2025-04-03 20:47:57 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,7 @@ import Header from "./components/Header";
 const App = () => {
   return (
     <>
-      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
      
-      </div>
     </>
   );
 };
