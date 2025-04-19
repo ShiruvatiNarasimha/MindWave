51569ab01f620e8192df4726faa5b99f0e3a240e
Shiruvati <mrnattyboy05@gmail.com>
2025-04-19 22:17:06 +0530

    Update Benefits.jsx

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -17,7 +17,6 @@ const Benefits = () => {
                 className="absolute inset-0.5 bg-n-8"
                 style={{ clipPath: "url(#benefits)" }}
               >
-                <div className="absolute inset-0 opacity-0 transition-opacity hover:opac
           ))}
         </div>
       </div>
