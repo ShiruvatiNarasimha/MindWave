f63eef6c78e4ddb5644116a0d8ac015bff273aee
Shiruvati <mrnattyboy05@gmail.com>
2025-05-10 22:02:24 +0530

    Update Benefits.jsx

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -1,8 +1,6 @@
 import { benefits } from "../constants";
 import Heading from "./Heading";
 import Section from "./Section";
-import Arrow from "../assets/svg/Arrow";
-import { GradientLight } from "./design/Benefits";
 
   return (
   );
