a96846341491e1e55981ab91d8bca1297fa22eae
Shiruvati <mrnattyboy05@gmail.com>
2025-05-18 18:52:59 +0530

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,6 @@
 import ButtonSvg from "../assets/svg/ButtonSvg";
 
 const Button = ({ className, href, onClick, childern, px, white }) => {
-  const classes = `button relative inline-flex items-center justify-center h-11 transition-colors hover:text-color-1 ${
     px || "px-7"
 
 
