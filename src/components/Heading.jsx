b8ef0c9614b75f2caa39e5d535f47c5562f11812
Shiruvati <mrnattyboy05@gmail.com>
2025-05-25 21:00:35 +0530

    Update Heading.jsx

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -6,7 +6,6 @@ const Heading = ({ className, title, text, tag }) => {
       className={`${className} max-w-[50rem] mx-auto mb-12 lg:mb-20 md:text-center`}
     >
       {tag && <TagLine className="mb-4 md:justify-center">{tag}</TagLine>}
-      {title && <h2 className="h2">{title}</h2>}
     </div>
   );
 };
