adacb085336265dcf45843d016eb646910b9ec23
Shiruvati <mrnattyboy05@gmail.com>
2025-05-25 21:00:54 +0530

    Update Heading.jsx

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -3,7 +3,6 @@ import TagLine from "./Tagline";
 const Heading = ({ className, title, text, tag }) => {
   return (
     <div
-      className={`${className} max-w-[50rem] mx-auto mb-12 lg:mb-20 md:text-center`}
     >
     </div>
   );
